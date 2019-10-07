05eadbb65ee908ad9efee52795f18b528d68dedf
40213151 <daimaruplay@gmail.com>
2019-10-07 09:55:38 +0900

    add sidebar.vue

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="sidebar">
+    <div class="sidebar__head">
+      <a class="sidebar__head__plus">
+        <i class="fas fa-plus plus"></i>
+      </a>
+    </div>
+    <div class="sidebar__list">
+      <div class="sidebar__list__group">
+        <div class="sidebar__list__group--name">
+          チャットグループ名
+        </div>
+        <div class="sidebar__list__group--number">
+          2
+        </div>
+      </div>
+      <div class="sidebar__list__group">
+        <div class="sidebar__list__group--name">
+          チャットグループ名
+        </div>
+      </div>
+      <div class="sidebar__list__group">
+        <div class="sidebar__list__group--name">
+          チャットグループ名
+        </div>
+      </div>
+      <div class="sidebar__list__group">
+        <div class="sidebar__list__group--name">
+          チャットグループ名
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+  .sidebar{
+    height: 100vh;
+    width: 330px;
+    background-color: gray;
+    font-size: 20px;
+    &__head{
+      height: 110px;
+      line-height: 110px;
+      padding-left: 30px;
+      &__plus{
+        border: 1px solid #fff;
+        padding: 10px;
+        border-radius: 50%;
+        color: #fff;
+      }
+    }
+    &__list{
+      height: calc(100vh - 110px;);
+      color: #fff;
+      padding-left: 30px;
+      &__group{
+        margin-bottom: 28px;
+        display: flex;
+        &--number{
+          margin-left: 23px;
+          background-color: #FF3F60;
+          padding: 1px 15px;
+          border-radius: 35%;
+        }
+      }
+    }
+  }
+</style>
