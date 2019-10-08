490e63f66f51cb2bf8f99d3ec3725b5c78c743b1
40213151 <daimaruplay@gmail.com>
2019-10-08 16:21:23 +0900

    create group action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, format: 'json' do
+    resources :groups, only: [:create, :update, :destroy]
+  end
 end
