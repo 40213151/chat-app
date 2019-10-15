a109f6002e2d571768326199db865a9887981f3a
40213151 <daimaruplay@gmail.com>
2019-10-15 17:32:14 +0900

    create methods

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -8,15 +8,28 @@ export function groupList(){
     return axios.get(localGroupUrl)
 }
 
-export function groupCreate(hoge){
+export function groupFind(id){
+    return  axios.get(localGroupUrl + `${id}`)
+}
+
+export function groupCreate(group){
     return axios
-            .post(localGroupUrl, hoge)
-            .then(response =>{
-              location.href = topUrl;
-            })
-            .catch(error => {
-              if(error.response.data && error.response.data.errors){
-                this.errors = error.response.data.errors;
-              }
-            });
+      .post(localGroupUrl, group)
+      .then(response =>{
+        location.href = topUrl;
+      })
+}
+
+export function groupUpdate(group){
+    return axios
+      .patch(localGroupUrl + `${group.id}`, group)
+      .then(response =>{
+        location.href = topUrl;
+      })
+}
+
+export function ErrorMessage(error,group){
+  if(error.response.data && error.response.data.errors){
+    group.errors = error.response.data.errors;
+  }
 }
